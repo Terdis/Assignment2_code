b5d75e48e696b0f1c755314b22aecc5489c63c3c
Fontana <umbertofontana@outlook.it>
2022-11-12 17:32:30 +0100

    html adding

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from datetime import datetime
 
 app=Flask(__name__)
-HELLO_HTML="""
-    <html><body>
-    <h1> Hello, {0}!</h1>
-    </body></html>
-"""
+
 @app.route('/')
 def hello():
-    name=request.args['min']
-    return HELLO_HTML.format(name, str(datetime.now()))
+   return render_template("index.html")
+
 
 @app.route('/echo/<name>')
 def echo(name):

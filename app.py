6d5fede8b7cb89a0623013a8162a2e72237835cf
Fontana <umbertofontana@outlook.it>
2022-11-12 17:00:50 +0100

    trying input

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
-from flask import Flask
+from flask import Flask, request
+from datetime import datetime
 
 app=Flask(__name__)
-
+HELLO_HTML="""
+    <html><body>
+    <h1> Hello, {0}!</h1>
+    </body></html>
+"""
 @app.route('/')
 def hello():
     return "hello Jova!"
 
 @app.route('/echo/<name>')
 def echo(name):
-    return f'Hello {name}'
+    name=request.args['min']
+    return HELLO_HTML.format(name, str(datetime.now()))
 
 if __name__=='__main__':
     app.run()

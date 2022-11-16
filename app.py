4c2878ef91e3838efb24e30c8dea5611eec03432
Fontana <umbertofontana@outlook.it>
2022-11-16 13:36:54 +0100

    removed datetime

--- app.py
+++ app.py
@@ -11,10 +11,7 @@ def hello():
 
 @app.route('/<min_x>/<max_x>')
 def echo(min_x, max_x):
-    initial=datetime.now()
     values=integral.sinIntegral(float(min_x), float(max_x))
-    final=datetime.now()
-    values[9]=str(final-initial)
     return jsonify(values)
 
 if __name__=='__main__':
